c38d04feccd4498f0ba9ccbbb93ff9b35e514240
YASSERRMD <arafath.yasser@gmail.com>
2026-06-25 00:19:02 +0400

    test(go-sdk): add agent test scaffold with makeAgent helper

---
+++ sdk/go/ancora/agent_test.go
@@ -0,0 +1,14 @@
+package ancora_test
+
+import (
+	"testing"
+
+	"ancora.io/sdk/ancora"
+)
+
+func makeAgent(t *testing.T) (*ancora.Runtime, *ancora.Agent) {
+	t.Helper()
+	rt := mustRuntime(t)
+	spec := ancora.NewAgentSpec("test", "llama3", "You are a test agent.")
+	return rt, ancora.NewAgent(rt, spec)
+}
