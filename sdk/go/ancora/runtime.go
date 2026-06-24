d3d6b6b0d73327264c9d8c64db0e494bd2c21d35
YASSERRMD <arafath.yasser@gmail.com>
2026-06-25 00:05:34 +0400

    feat(go-sdk): add NewRuntime constructor

--- sdk/go/ancora/runtime.go
+++ sdk/go/ancora/runtime.go
@@ -1,9 +1,23 @@
 package ancora
 
+import "runtime"
+
 // #include "ancora.h"
 import "C"
 
 // Runtime is an opaque handle to an Ancora runtime instance.
 type Runtime struct {
 	ptr *C.AncorRuntime
+}
+
+// NewRuntime allocates a new runtime. Returns an error if allocation fails.
+func NewRuntime() (*Runtime, error) {
+	var ptr *C.AncorRuntime
+	code := C.ancora_runtime_new(&ptr)
+	if err := asError(code); err != nil {
+		return nil, err
+	}
+	r := &Runtime{ptr: ptr}
+	runtime.SetFinalizer(r, (*Runtime).Free)
+	return r, nil
 }
