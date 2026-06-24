37492c3a86a0accc2489523d7bcc17a67e78850f
YASSERRMD <arafath.yasser@gmail.com>
2026-06-25 00:19:11 +0400

    test(go-sdk): NewAgent returns non-nil

--- sdk/go/ancora/agent_test.go
+++ sdk/go/ancora/agent_test.go
@@ -12,3 +12,11 @@ func makeAgent(t *testing.T) (*ancora.Runtime, *ancora.Agent) {
 	spec := ancora.NewAgentSpec("test", "llama3", "You are a test agent.")
 	return rt, ancora.NewAgent(rt, spec)
 }
+
+func TestNewAgentReturnsNonNil(t *testing.T) {
+	rt, ag := makeAgent(t)
+	defer rt.Free()
+	if ag == nil {
+		t.Fatal("NewAgent returned nil")
+	}
+}
