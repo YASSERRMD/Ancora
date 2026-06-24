06dc602d512d341c3f8d1ef0771c0a8933e7ad15
YASSERRMD <arafath.yasser@gmail.com>
2026-06-25 02:07:01 +0400

    chore(ts-sdk): update jest.config.js to ts-jest v29 transform syntax

--- sdk/ts/jest.config.js
+++ sdk/ts/jest.config.js
@@ -1,11 +1,8 @@
 /** @type {import('jest').Config} */
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/__tests__/**/*.test.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.json',
-    },
+  transform: {
+    '^.+\\.tsx?$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
   },
 }
