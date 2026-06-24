4e19e758e2a5fdd1b967f58cbcc5f5501ec493b6
YASSERRMD <arafath.yasser@gmail.com>
2026-06-25 00:30:57 +0400

    build(go-sdk): add grpc generate directive to gen.go

--- sdk/go/gen.go
+++ sdk/go/gen.go
@@ -1,3 +1,4 @@
 //go:generate protoc --proto_path=../../crates/ancora-proto/proto --go_out=. --go_opt=module=github.com/YASSERRMD/Ancora/bindings/go ../../crates/ancora-proto/proto/messages.proto ../../crates/ancora-proto/proto/contracts.proto ../../crates/ancora-proto/proto/journal.proto ../../crates/ancora-proto/proto/errors.proto
+//go:generate protoc --proto_path=../../crates/ancora-grpc/proto --go_out=ancora/grpc/ --go_opt=paths=source_relative --go-grpc_out=ancora/grpc/ --go-grpc_opt=paths=source_relative ../../crates/ancora-grpc/proto/ancora.proto
 
 package sdk
